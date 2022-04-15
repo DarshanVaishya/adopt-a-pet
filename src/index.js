f8bea2e343c656c4646e9281dff741991821bdce
Darshan Vaishya <DarshanVaishya3@gmail.com>
2022-04-15 11:13:00 +0530

    Feature: Added react-modal

--- src/index.js
+++ src/index.js
@@ -1,16 +1,25 @@
 import React, { useEffect, useState } from "react";
 import { createRoot } from "react-dom/client";
+import Modal from "react-modal";
 import Pet from "./Pet";
 import "./index.css";
 
 const App = () => {
 	const [pets, setPets] = useState([]);
+	const [isOpen, setOpen] = useState(false);
+	const [isLoading, setLoading] = useState(false);
 
 	useEffect(() => {
 		async function getData() {
-			const res = await fetch("http://localhost:3001/pets");
-			const data = await res.json();
-			setPets(data);
+			try {
+				setLoading(true);
+				const res = await fetch("http://localhost:3001/pets");
+				const data = await res.json();
+				setPets(data);
+				setLoading(false);
+			} catch (e) {
+				setLoading(false);
+			}
 		}
 		getData();
 	}, []);
@@ -18,17 +27,29 @@ const App = () => {
 	return (
 		<main>
 			<h1>Adopt-a-Pet</h1>
-			<ul>
-				{pets.map((pet) => (
-					<li key={pet.id}>
-						<Pet pet={pet} />
-					</li>
-				))}
-			</ul>
-			<button>Add a Pet</button>
+			{isLoading ? (
+				<div className="loading">Loading...</div>
+			) : (
+				<>
+					<ul>
+						{pets.map((pet) => (
+							<li key={pet.id}>
+								<Pet pet={pet} />
+							</li>
+						))}
+					</ul>
+					<button onClick={() => setOpen(true)}>Add a Pet</button>
+				</>
+			)}
+			<Modal isOpen={isOpen} onRequestClose={() => setOpen(false)}>
+				Hello
+			</Modal>
 		</main>
 	);
 };
 
-const root = createRoot(document.querySelector("#root"));
+const rootEl = document.querySelector("#root");
+Modal.setAppElement(rootEl);
+
+const root = createRoot(rootEl);
 root.render(<App />);
