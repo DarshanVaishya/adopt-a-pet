925c322274d352333747e0f77b42edaffd345f79
Darshan Vaishya <DarshanVaishya3@gmail.com>
2022-04-15 10:52:52 +0530

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import Pet from "./Pet";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const App = () => {
+	const [pets, setPets] = useState([]);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+	return (
+		<main>
+			<h1>Adopt-a-Pet</h1>
+			<ul>
+				{pets.map((pet) => (
+					<li key={pet.id}>
+						<Pet pet={pet} />
+					</li>
+				))}
+			</ul>
+			<button>Add a Pet</button>
+		</main>
+	);
+};
+
+ReactDOM.render(<App />, document.querySelector("#root"));
